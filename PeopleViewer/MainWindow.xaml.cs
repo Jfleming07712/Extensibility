e5c5daf57b008f405fce15e330611fa3e5409957
Jfleming <Jfleming07712@gmail.com>
2019-06-17 15:52:03 -0400

    Extensibility demo done for mod 3 in interface tutorial.

--- PeopleViewer/MainWindow.xaml.cs
+++ PeopleViewer/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using PersonRepository.Interface;
+﻿using PersonRepository.CSV;
+using PersonRepository.Factory;
+using PersonRepository.Interface;
+using PersonRepository.Service;
+using PersonRepository.SQL;
 using System.Windows;
 
 namespace PeopleViewer
@@ -12,17 +16,30 @@ namespace PeopleViewer
 
         private void ServiceFetchButton_Click(object sender, RoutedEventArgs e)
         {
-
+            PopulateListBox("Service");
         }
 
         private void CSVFetchButton_Click(object sender, RoutedEventArgs e)
         {
-
+            PopulateListBox("CSV");
         }
 
         private void SQLFetchButton_Click(object sender, RoutedEventArgs e)
         {
+            PopulateListBox("SQL");
+        }
 
+        private void PopulateListBox(string repositoryType)
+        {
+            ClearListBox();
+
+            IPersonRepository repository = RepositoryFactory.GetRepository(repositoryType);
+
+            var people = repository.GetPeople();
+            foreach (var person in people)
+                PersonListBox.Items.Add(person);
+
+            ShowRepositoryType(repository);
         }
 
         private void ClearButton_Click(object sender, RoutedEventArgs e)
